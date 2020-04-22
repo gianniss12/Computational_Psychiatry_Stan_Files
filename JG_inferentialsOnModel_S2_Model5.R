ced57513924e5556d33fe7c489672d36157e44d8
jvgiordano <jgiordano100@gmail.com>
2020-04-22 10:20:26 +0100

    Created Inferential Files specific to Model 5 final analysis on Dataset S2. Created a Model 5 fitting with no Perseverance parameter (ie no stickiness Beta).

--- JG_inferentialsOnModel_S2_Model5.R
+++ JG_inferentialsOnModel_S2_Model5.R
@@ -24,12 +24,6 @@ launch_shinystan(my_shinystan)
 ### Derive subject means for Betas, Alpha #######################
 m = extract(fit, permuted=TRUE)
 
-### For Single Alpha ##########################
-bySubj = data.frame(subj=unique(data$subj), betac=rep(0,(length(unique(data$subj)))), alpha1=rep(0,(length(unique(data$subj)))), beta1m=rep(0,(length(unique(data$subj)))), beta1t=rep(0,(length(unique(data$subj)))),beta2=rep(0,(length(unique(data$subj)))))
-
-for( i in 1:length(unique(data$subj))){
-  bySubj[i,2:6] = c(mean(m$betac[,i]),mean(m$alpha1[,i]), mean(m$beta1m[,i]),mean(m$beta1t[,i]), mean(m$beta2[,i]))
-}
 
 ### For Multiple Alphas, Both Stages ##########################
 bySubj = data.frame(subj=unique(data$subj), betac=rep(0,(length(unique(data$subj)))), alpha_p=rep(0,(length(unique(data$subj)))), alpha_n=rep(0,(length(unique(data$subj)))), beta1m=rep(0,(length(unique(data$subj)))), beta1t=rep(0,(length(unique(data$subj)))),beta2=rep(0,(length(unique(data$subj)))))
@@ -37,6 +31,10 @@ for( i in 1:length(unique(data$subj))){
   bySubj[i,2:7] = c(mean(m$betac[,i]),mean(m$alpha_p[,i]), mean(m$alpha_n[,i]), mean(m$beta1m[,i]),mean(m$beta1t[,i]), mean(m$beta2[,i]))
 }
 summary(bySubj)
+
+## Show Mean Value Histograms
+hist(bySubj$alpha_p)
+hist(bySubj$alpha_n)
 
 ## Checking 2.5% Quantile
 bySubj_2.5 = data.frame(subj=unique(data$subj), betac=rep(0,(length(unique(data$subj)))), alpha_p=rep(0,(length(unique(data$subj)))), alpha_n=rep(0,(length(unique(data$subj)))), beta1m=rep(0,(length(unique(data$subj)))), beta1t=rep(0,(length(unique(data$subj)))),beta2=rep(0,(length(unique(data$subj)))))
@@ -52,29 +50,9 @@ for( i in 1:length(unique(data$subj))){
 }
 summary(bySubj_97.5)
 
-### For Multiple Alphas, Seperate Stages ############################
-
-## Calculate Mean
-bySubj = data.frame(subj=unique(data$subj), betac=rep(0,(length(unique(data$subj)))), alpha1_p=rep(0,(length(unique(data$subj)))), alpha1_n=rep(0,(length(unique(data$subj)))), alpha2_p=rep(0,(length(unique(data$subj)))), alpha2_n=rep(0,(length(unique(data$subj)))), beta1m=rep(0,(length(unique(data$subj)))), beta1t=rep(0,(length(unique(data$subj)))),beta2=rep(0,(length(unique(data$subj)))))
-for( i in 1:length(unique(data$subj))){
-  bySubj[i,2:9] = c(mean(m$betac[,i]),mean(m$alpha1_p[,i]), mean(m$alpha1_n[,i]), mean(m$alpha2_p[,i]), mean(m$alpha2_n[,i]), mean(m$beta1m[,i]),mean(m$beta1t[,i]), mean(m$beta2[,i]))
-}
-summary(bySubj)
-
-## Checking 2.5% Quantile
-bySubj_2.5 = data.frame(subj=unique(data$subj), betac=rep(0,(length(unique(data$subj)))), alpha1_p=rep(0,(length(unique(data$subj)))), alpha1_n=rep(0,(length(unique(data$subj)))), alpha2_p=rep(0,(length(unique(data$subj)))), alpha2_n=rep(0,(length(unique(data$subj)))), beta1m=rep(0,(length(unique(data$subj)))), beta1t=rep(0,(length(unique(data$subj)))),beta2=rep(0,(length(unique(data$subj)))))
-for( i in 1:length(unique(data$subj))){
-  bySubj_2.5[i,2:9] = c(quantile(m$betac[,i], probs = c(.025)),quantile(m$alpha1_p[,i], probs = c(.025)), quantile(m$alpha1_n[,i], probs = c(.025)), quantile(m$alpha2_p[,i], probs = c(.025)), quantile(m$alpha2_n[,i], probs = c(.025)), quantile(m$beta1m[,i], probs = c(.025)),quantile(m$beta1t[,i], probs = c(.025)), quantile(m$beta2[,i], probs = c(.025)))
-}
-summary(bySubj_2.5)
 
 
-##Checking 97.5% Quantile
-bySubj_97.5 = data.frame(subj=unique(data$subj), betac=rep(0,(length(unique(data$subj)))), alpha1_p=rep(0,(length(unique(data$subj)))), alpha1_n=rep(0,(length(unique(data$subj)))), alpha2_p=rep(0,(length(unique(data$subj)))), alpha2_n=rep(0,(length(unique(data$subj)))), beta1m=rep(0,(length(unique(data$subj)))), beta1t=rep(0,(length(unique(data$subj)))),beta2=rep(0,(length(unique(data$subj)))))
-for( i in 1:length(unique(data$subj))){
-  bySubj_97.5[i,2:9] = c(quantile(m$betac[,i], probs = c(.975)),quantile(m$alpha1_p[,i], probs = c(.975)), quantile(m$alpha1_n[,i], probs = c(.975)), quantile(m$alpha2_p[,i], probs = c(.975)), quantile(m$alpha2_n[,i], probs = c(.975)), quantile(m$beta1m[,i], probs = c(.975)),quantile(m$beta1t[,i], probs = c(.975)), quantile(m$beta2[,i], probs = c(.975)))
-}
-summary(bySubj_97.5)
+
 
 
 ## Load in Psychiatric Scores #####################
@@ -91,23 +69,13 @@ comb = merge(sreps, bySubj, by="subj")
 
 
 symptoms = c("sds","stai","oci", "lsas", "bis", "scz", "aes", "eat", "audit")
-coeff = rep(0,9)
-
-symp_p = data.frame(symptoms, coeff)
-symp_n = data.frame(symptoms, coeff)
-
-
 factors = c("Factor1", "Factor2", "Factor3")
-coeff = rep(0,3)
-fact = data.frame(factors, coeff)
+
 
 
 
 ### Analysis for 2 Learning Rates ###
 ## Analysis - Alpha_p
-
-#Create list of scales, zerod coeff
-symptoms = c("sds","stai","oci", "lsas", "bis", "scz", "aes", "eat", "audit")
 
 coeff = rep(0,9)
 coeff[1] = summary(lm(alpha_p ~ scale(iq) + scale(age) + gender + scale(sds_total), data=comb))$coefficients[5,1]
@@ -121,6 +89,7 @@ coeff[8] = summary(lm(alpha_p ~ scale(iq) + scale(age) + gender + scale(eat_tota
 coeff[9] = summary(lm(alpha_p ~ scale(iq) + scale(age) + gender + scale(audit_total), data=comb))$coefficients[5,1]
 coeff = coeff*100
 symp_p = data.frame(symptoms, coeff)
+symp_p = symp_p[order(symp_p$coeff),]
 
 coeff = rep(0,3)
 coeff[1] = summary(lm(alpha_p ~ scale(iq) + scale(age) + gender + scale(Factor1), data=comb))$coefficients[5,1]
@@ -130,7 +99,7 @@ coeff = coeff*100
 fact_p = data.frame(factors, coeff)
 
 
-##########
+### Summaries
 
 summary(lm(alpha_p ~ scale(iq) + scale(age) + gender + scale(sds_total), data=comb))
 summary(lm(alpha_p ~ scale(iq) + scale(age) + gender + scale(stai_total), data=comb))
@@ -164,6 +133,7 @@ coeff[8] = summary(lm(alpha_n ~ scale(iq) + scale(age) + gender + scale(eat_tota
 coeff[9] = summary(lm(alpha_n ~ scale(iq) + scale(age) + gender + scale(audit_total), data=comb))$coefficients[5,1]
 coeff = coeff*100
 symp_n = data.frame(symptoms, coeff)
+symp_n = symp_n[order(symp_n$coeff),]
 
 coeff = rep(0,3)
 coeff[1] = summary(lm(alpha_n ~ scale(iq) + scale(age) + gender + scale(Factor1), data=comb))$coefficients[5,1]
@@ -171,6 +141,24 @@ coeff[2] = summary(lm(alpha_n ~ scale(iq) + scale(age) + gender + scale(Factor2)
 coeff[3] = summary(lm(alpha_n ~ scale(iq) + scale(age) + gender + scale(Factor3), data=comb))$coefficients[5,1]
 coeff = coeff*100
 fact_n = data.frame(factors, coeff)
+
+### Summaries
+
+summary(lm(alpha_n ~ scale(iq) + scale(age) + gender + scale(sds_total), data=comb))
+summary(lm(alpha_n ~ scale(iq) + scale(age) + gender + scale(stai_total), data=comb))
+summary(lm(alpha_n ~ scale(iq) + scale(age) + gender + scale(oci_total), data=comb))
+summary(lm(alpha_n ~ scale(iq) + scale(age) + gender + scale(lsas_total), data=comb))
+summary(lm(alpha_n ~ scale(iq) + scale(age) + gender + scale(bis_total), data=comb))
+summary(lm(alpha_n ~ scale(iq) + scale(age) + gender + scale(scz_total), data=comb))
+summary(lm(alpha_n ~ scale(iq) + scale(age) + gender + scale(aes_total), data=comb))
+summary(lm(alpha_n ~ scale(iq) + scale(age) + gender + scale(eat_total), data=comb))
+summary(lm(alpha_n ~ scale(iq) + scale(age) + gender + scale(audit_total), data=comb))
+
+
+
+summary(lm(alpha_n ~ scale(iq) + scale(age) + gender + scale(Factor1), data=comb))
+summary(lm(alpha_n ~ scale(iq) + scale(age) + gender + scale(Factor2), data=comb))
+summary(lm(alpha_n ~ scale(iq) + scale(age) + gender + scale(Factor3), data=comb))
 
 
 ## Analysis - [alpha_p - alpha_n] / [alpha_p + alpha_n]
@@ -191,7 +179,7 @@ summary(lm(((alpha_p-alpha_n)/(alpha_p+alpha_n)) ~ scale(iq) + scale(age) + gend
 
 
 ### Plotting ################
-
+symp_p$symptoms = with(symp_p, reorder(symptoms, coeff))
 ggplot(data=symp_p, aes(x=symptoms, y=coeff, fill=symptoms)) +
   geom_bar(colour="black", stat="identity") +
   xlab("Clinical Scales") + ylab("Percent Change in Positive Learning") +
@@ -204,6 +192,7 @@ ggplot(data=fact_p, aes(x=factors, y=coeff, fill=factors)) +
   ggtitle("Percentage Change in Positive Learning By Factor") +
   guides(fill=FALSE) 
   
+symp_n$symptoms = with(symp_n, reorder(symptoms, coeff))
 ggplot(data=symp_n, aes(x=symptoms, y=coeff, fill=symptoms)) +
   geom_bar(colour="black", stat="identity") +
   xlab("Clinical Scales") + ylab("Percent Change in Negative Learning") +
